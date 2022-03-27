62925a24d7b6763427f4689da984245c08318ea2
김상현 <kshy1019@naver.com>
2022-03-27 16:51:07 +0900

    feat: extension 클래스

---
+++ MoyeoRun/Scenes/TabBar/ViewControllers/TabBarViewControllers.swift
@@ -0,0 +1,27 @@
+//
+//  TabBarViewControllers.swift
+//  MoyeoRun
+//
+//  Created by 김상현 on 2022/03/27.
+//
+
+
+import Foundation
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+    
+    func setUI() {
+       setTabBarShadow()
+    }
+    
+    func setTabBarShadow(){
+        UITabBar.clearShadow()
+        tabBar.layer.applyShadow(color: .gray, alpha: 0.3, x: 0, y: 0, blur: 12)
+    }
+}
