a9a7aa1a0ccc90a8cd90e726e9902d809279e5ac
jdyj <jdyj@naver.com>
2022-03-29 17:20:01 +0900

    feat(MapKitView): MapKitView 생성
    #5

---
+++ MoyeoRun/Util/Components/MapKitView.swift
@@ -0,0 +1,20 @@
+//
+//  MapKitView.swift
+//  MoyeoRun
+//
+//  Created by 조재영 on 2022/03/29.
+//
+
+import UIKit
+
+class MapKitView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
