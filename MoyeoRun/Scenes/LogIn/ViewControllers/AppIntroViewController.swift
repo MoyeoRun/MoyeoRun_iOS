83862f96b3262a480b9a1a0a6187eca7cac99f76
김상현 <kshy1019@naver.com>
2022-04-30 14:15:24 +0900

    feat: 앱 시작화면 설정, 앱 시작흐름(로그인) 설정

--- MoyeoRun/Scenes/LogIn/ViewControllers/AppIntroViewController.swift
+++ MoyeoRun/Scenes/LogIn/ViewControllers/AppIntroViewController.swift
@@ -8,22 +8,14 @@
 import UIKit
 
 class AppIntroViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onTapStartButton(_ sender: Any) {
+        let storyBoard = UIStoryboard(name: "SocialLogIn", bundle: nil)
+        let viewController = storyBoard.instantiateViewController(withIdentifier: "SocialLogIn")
+        viewController.modalPresentationStyle = .fullScreen
+        self.present(viewController, animated: false)
     }
-    */
-
 }
--- MoyeoRun/Scenes/LogIn/ViewControllers/SocialLogInViewController.swift
+++ MoyeoRun/Scenes/LogIn/ViewControllers/SocialLogInViewController.swift
@@ -8,24 +8,27 @@
 import UIKit
 
 class SocialLogInViewController: UIViewController {
+    @IBOutlet weak var signInWithGoogleButton: UIButton!
+    @IBOutlet weak var appleSymbolImageView: UIImageView!
 
-    @IBOutlet weak var SignInWithGoogleButton: UIButton!
-    @IBOutlet weak var AppleSymbolImageView: UIImageView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
     }
-    func setUI(){
+
+    func setUI() {
         setSignInWithGoogleButton()
-        
-        func setSignInWithAppleButton(){
-            AppleSymbolImageView.layer.cornerRadius = 4
+
+        func setSignInWithAppleButton() {
+            appleSymbolImageView.layer.cornerRadius = 4
         }
-        func setSignInWithGoogleButton(){
-            let roboto = UIFont(name: "Roboto-Regular", size: 18)
-            
-            SignInWithGoogleButton.titleLabel?.font = roboto
+        func setSignInWithGoogleButton() {
         }
+    }
+
+    @IBAction func onTapSignInWithAppleButton(_ sender: Any) {
+    }
+
+    @IBAction func onTapSignInWithGoogleButton(_ sender: Any) {
     }
 }
