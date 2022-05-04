27c30f40756053711d99619dc1626ba786240116
moondalnim <96918965+moondalnim@users.noreply.github.com>
2022-05-04 19:55:06 +0900

    feat: API 통신시 사용되는 토큰 관리를 위한 LocalDataSouce 코드 작성
    API 통신시 사용되는 토큰 관리를 위한 LocalDataSouce 코드 작성
    
    Refs: #17

---
+++ MoyeoRun/Data/Auth/DataSource/AuthLocalDataSource.swift
@@ -0,0 +1,60 @@
+//
+//  AuthLocalDataSource.swift
+//  MoyeoRun
+//
+//  Created by Jeongho Moon on 2022/05/04.
+//
+
+import Foundation
+
+protocol AuthLocalDataSourceable {
+    init(store: KeychainStorable)
+
+    func storeToken(token: TokenResponse) -> Result<Void, Error>
+
+    func refreshAccessToken(accessToken: String) -> Result<Void, Error>
+
+    func clearToken() -> Result<Void, Error>
+}
+
+enum AuthKey: String, KeychainKeyType {
+    case accessToken
+    case refreshToken
+}
+
+final class AuthLocalDataSource: AuthLocalDataSourceable {
+    let store: KeychainStorable
+
+    required init(store: KeychainStorable = KeychainStore()) {
+        self.store = store
+    }
+    
+    func storeToken(token: TokenResponse) -> Result<Void, Error> {
+        do {
+            try store.create(value: token.accessToken, forKey: AuthKey.accessToken)
+            try store.create(value: token.refreshToken, forKey: AuthKey.refreshToken)
+            return .success(())
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    func refreshAccessToken(accessToken: String) -> Result<Void, Error> {
+        do {
+            try store.update(value: accessToken, forKey: AuthKey.accessToken)
+            return .success(())
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    func clearToken() -> Result<Void, Error> {
+        do {
+            try store.delete(forKey: AuthKey.accessToken)
+            try store.delete(forKey: AuthKey.refreshToken)
+            return .success(())
+        } catch {
+            return .failure(error)
+        }
+    }
+}
