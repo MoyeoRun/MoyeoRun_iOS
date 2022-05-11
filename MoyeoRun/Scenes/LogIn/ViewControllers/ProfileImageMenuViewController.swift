956d0b49c74aac76b5d18cd3faae563af1ad6ff0
김상현 <kshy1019@naver.com>
2022-05-11 22:19:27 +0900

    style: 주석 제거

--- MoyeoRun/Scenes/LogIn/ViewControllers/ProfileImageMenuViewController.swift
+++ MoyeoRun/Scenes/LogIn/ViewControllers/ProfileImageMenuViewController.swift
@@ -41,17 +41,10 @@ class ProfileImageMenuViewController: UIViewController {
 
     @IBAction func onTapSelectOnGallery(_ sender: Any) {
         let config = YPImagePickerConfiguration()
-        /// [Edit configuration here ...]
-        /// Build a picker with your configuration
         let picker = YPImagePicker(configuration: config)
 
         picker.didFinishPicking { [unowned picker] items, _ in
             if let photo = items.singlePhoto {
-                /// print(photo.fromCamera) // Image source (camera or library)
-                /// print(photo.image) // Final image selected by the user
-                /// print(photo.originalImage) // original image selected by the user, unfiltered
-                /// print(photo.modifiedImage) // Transformed image, can be nil
-                /// print(photo.exifMeta) // Print exif meta data of original image.
             }
             picker.dismiss(animated: true, completion: nil)
         }
