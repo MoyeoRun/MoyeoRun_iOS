133f01c54536c9eed58e5546cc6ceb9eac16e83a
김상현 <kshy1019@naver.com>
2022-05-14 23:06:20 +0900

    feat: SearchBar 꾸미기

--- MoyeoRun/Scenes/SearchTab/ViewControllers/SearchTabViewController.swift
+++ MoyeoRun/Scenes/SearchTab/ViewControllers/SearchTabViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class SearchTabViewController: UIViewController {
-
+    @IBOutlet weak var searchBar: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUI()
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUI() {
+        setSearchBarUI()
+        func setSearchBarUI() {
+            searchBar.layer.borderWidth = 1
+            searchBar.layer.borderColor = UIColor.black.cgColor
+            searchBar.layer.cornerRadius = 4
+        }
     }
-    */
-
 }
