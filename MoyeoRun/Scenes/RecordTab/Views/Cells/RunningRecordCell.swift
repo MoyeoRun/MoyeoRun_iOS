619a7b13b454023a67f9847f8b7af681e7d010ef
김상현 <kshy1019@naver.com>
2022-05-24 22:25:42 +0900

    feat: profileImage collectionview 추가

--- MoyeoRun/Scenes/RecordTab/Views/Cells/RunningRecordCell.swift
+++ MoyeoRun/Scenes/RecordTab/Views/Cells/RunningRecordCell.swift
@@ -10,8 +10,41 @@ import UIKit
 
 class RunningRecordCell: UICollectionViewCell {
     @IBOutlet var imageView: UIImageView!
+    @IBOutlet var adminProfileImageView: UIImageView!
+
     func setUI() {
         imageView.layer.cornerRadius = 4
+        adminProfileImageView.layer.cornerRadius = adminProfileImageView.layer.bounds.width * 0.5
         self.layer.cornerRadius = 4
+    }
+}
+
+extension RunningRecordCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "ProfileImageCell", for: indexPath
+        ) as? ProfileImageCell else {
+            return UICollectionViewCell()
+        }
+        cell.setUI()
+
+        return cell
+    }
+}
+
+extension RunningRecordCell: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 25, height: 25)
+    }
+}
+
+class ProfileImageCell: UICollectionViewCell {
+    @IBOutlet var profileImageView: UIImageView!
+    func setUI() {
+        profileImageView.layer.cornerRadius = profileImageView.bounds.width * 0.5
     }
 }
