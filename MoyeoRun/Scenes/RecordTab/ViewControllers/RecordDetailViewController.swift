60c986851f587f5af6d7699c5787e160e203a26d
김상현 <kshy1019@naver.com>
2022-06-22 14:01:44 +0900

    [#65] 기록 탭 상세정보 페이지 UI작업 -2
    상단 UI 두줄 완성

--- MoyeoRun/Scenes/RecordTab/ViewControllers/RecordDetailViewController.swift
+++ MoyeoRun/Scenes/RecordTab/ViewControllers/RecordDetailViewController.swift
@@ -9,14 +9,63 @@ import UIKit
 
 class RecordDetailViewController: UIViewController {
     @IBOutlet var backgroundGrayView: UIView!
+    @IBOutlet var myProfileImageView: UIImageView!
+    @IBOutlet var myProfileNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setUI()
+        DispatchQueue.main.async {
+            self.setUI()
+        }
     }
 
     func setUI() {
         backgroundGrayView.layer.cornerRadius = 4
+        myProfileImageView.clipsToBounds = true
+        myProfileImageView.layer.cornerRadius = myProfileImageView.bounds.height * 0.5
+        myProfileImageView.layer.borderWidth = 2.5
+        myProfileImageView.layer.borderColor = UIColor(named: "RunBlue")?.cgColor
+    }
+}
+
+extension RecordDetailViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "RecordDetailCollectionViewCell", for: indexPath
+        ) as? RecordDetailCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+
+        DispatchQueue.main.async {
+            cell.setUI()
+        }
+
+        return cell
+    }
+}
+
+extension RecordDetailViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenWidth = UIScreen.main.bounds.width
+        let cellWidth = screenWidth * 0.15
+        let cellHeight = cellWidth + 4 + myProfileNameLabel.bounds.height
+
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+}
+
+class RecordDetailCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var userProfileImageView: UIImageView!
+    @IBOutlet var userProfileNameLabel: UILabel!
+
+    func setUI() {
+        self.userProfileImageView.image = UIImage(named: "people1")
+        self.userProfileImageView.clipsToBounds = true
+        self.userProfileImageView.layer.cornerRadius = self.userProfileImageView.bounds.width * 0.5
     }
 }
